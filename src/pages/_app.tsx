b2ab97d5b3ca45989cb78d3391b40c689444151b
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-03-03 20:45:23 -0300

    fix(font): removed warning in log related to importing fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,6 @@ import Layout from '@/components/Layout';
 config.autoAddCss = false;
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
-
   * {
     font-size: 16px;
   }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,12 @@ export default function Document() {
   return (
     <Html lang='pt-BR'>
       <Head />
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap'
+        rel='stylesheet'
+      />
       <body>
         <Main />
         <NextScript />
