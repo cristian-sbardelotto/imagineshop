063bbe6e22df415f97abe0362d4e8b1c565fff4b
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-03-08 16:14:55 -0300

    feat(shopping-cart): finished the main style to the route

--- src/pages/shopping-cart.tsx
+++ src/pages/shopping-cart.tsx
@@ -18,6 +18,15 @@ const ShoppingCart = () => {
     setProducts(values);
   }, []);
 
+  const isPlural = () => {
+    // checks if the products in the shopping cart are in the plural
+    if (products.length > 1) {
+      return 'produtos';
+    }
+
+    return 'produto';
+  };
+
   return products && products.length > 0 ? (
     <Main>
       <Title>Meu carrinho</Title>
@@ -42,8 +51,8 @@ const ShoppingCart = () => {
                     <Image
                       src={product.image}
                       alt='Product Image'
-                      width={200}
-                      height={200}
+                      width={180}
+                      height={180}
                     />
                   </div>
 
@@ -56,6 +65,42 @@ const ShoppingCart = () => {
               </div>
             ))}
         </ShoppingCartProducts>
+
+        <section>
+          <ShoppingCartPayment>
+            <PaymentTitle>1. Resumo do pedido</PaymentTitle>
+            <PaymentValue>
+              <span>
+                {products.length} {isPlural()}
+              </span>{' '}
+              <span>R$ 0.000</span>
+            </PaymentValue>
+
+            <PaymentShipping>
+              <span>Frete</span> <span>R$ 0.000</span>
+            </PaymentShipping>
+
+            <PaymentTotal>
+              <span>Total</span> <span>R$ 0.000</span>
+            </PaymentTotal>
+
+            <Separator />
+
+            <LoginTitle></LoginTitle>
+
+            <InputGroup>
+              <span>E-MAIL:</span>
+              <input type='text' />
+            </InputGroup>
+
+            <InputGroup>
+              <span>SENHA</span>
+              <input type='password' />
+            </InputGroup>
+
+            <Button>Continuar</Button>
+          </ShoppingCartPayment>
+        </section>
       </ShoppingCartContainer>
     </Main>
   ) : (
@@ -122,7 +167,8 @@ const Product = styled.div`
 `;
 
 const ProductName = styled.p`
-  padding: 1rem;
+  padding: 0;
+  margin: 0;
 
   font-size: 1rem;
   color: ${({ theme }) => theme.colors.secondary};
@@ -130,13 +176,118 @@ const ProductName = styled.p`
 `;
 
 const ProductPrice = styled.p`
-  padding: 1.125rem;
-  margin-top: 1rem;
+  padding: 0;
+  margin: 0;
 
   place-self: start end;
 
   font-size: 1rem;
   font-weight: 700;
+`;
+
+const ShoppingCartPayment = styled.div`
+  padding: 1rem;
+
+  background-color: #f0f1ef;
+  border-radius: 4px;
+`;
+
+const PaymentTitle = styled.p`
+  margin: 0;
+
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+
+const PaymentValue = styled.div`
+  margin: 5rem 0 0 0;
+  display: flex;
+  justify-content: space-between;
+
+  font-size: 1rem;
+  font-weight: 300;
+  color: ${({ theme }) => theme.colors.secondary};
+
+  span {
+    display: block;
+  }
+`;
+
+const PaymentShipping = styled.div`
+  margin: 1.5rem 0 0 0;
+  display: flex;
+  justify-content: space-between;
+
+  font-size: 1rem;
+  font-weight: 300;
+  color: ${({ theme }) => theme.colors.secondary};
+
+  span {
+    display: block;
+  }
+`;
+
+const PaymentTotal = styled.div`
+  margin: 3.125rem 0 5rem 0;
+  display: flex;
+  justify-content: space-between;
+
+  font-size: 1.125rem;
+  font-weight: 700;
+  text-transform: uppercase;
+
+  span {
+    display: block;
+  }
+`;
+
+const LoginTitle = styled.p`
+  margin: 2rem 0;
+
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+
+const InputGroup = styled.div`
+  margin-bottom: 1.3rem;
+
+  span {
+    margin-bottom: 0.625rem;
+    display: block;
+
+    text-transform: uppercase;
+  }
+
+  input {
+    width: 100%;
+    height: 1.25rem;
+
+    border: unset;
+  }
+`;
+
+const Button = styled.button`
+  display: block;
+  margin: 0 auto;
+  width: 240px;
+  height: 60px;
+
+  border: unset;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: #fff;
+  font-weight: 700;
+  font-size: 1.5rem;
+  font-family: 'Montserrat', sans-serif;
+  text-transform: uppercase;
+
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.85);
+  }
 `;
 
 export default ShoppingCart;
